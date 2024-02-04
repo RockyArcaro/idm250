722cfd721fd0df3c2ddeff730468c37fde34fad8
RockyArcaro <rockyarcaro3@gmail.com>
2024-02-04 16:05:28 -0500

    Created Dynamic header/footer

---
+++ wp-content/themes/idm-250-Portfolio/footer.php
@@ -0,0 +1,7 @@
+<footer style="background-color:black; color:white;"> 
+    &copy; 2024 | IDM 250
+</footer>
+
+</body>
+
+</html>
---
+++ wp-content/themes/idm-250-Portfolio/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>DOCUMENT</title>
+</head>
+
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">GRAPHICS</a></li>
+            <li><a href="#">UX DESIGN</a></li>
+            <img src="images/logo.png" alt="logo">
+            <li><a href="#">ABOUT ME</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+</header>
