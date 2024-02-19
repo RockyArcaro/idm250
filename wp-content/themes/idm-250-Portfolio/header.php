e54a8a991f93f94dc2d6c185d21dbbdb371466d5
RockyArcaro <114040029+RockyArcaro@users.noreply.github.com>
2024-02-18 21:41:21 -0500

    Update header.php

--- wp-content/themes/idm-250-Portfolio/header.php
+++ wp-content/themes/idm-250-Portfolio/header.php
@@ -19,11 +19,12 @@
 
 <header>
     <nav>
-        <ul>
-            <li><a href="#">GRAPHICS</a></li>
-            <li><a href="#">UX DESIGN</a></li>
-            <img src="images/logo.png" alt="logo">
-            <li><a href="#">ABOUT ME</a></li>
-            <li><a href="#">SHOP</a></li>
-        </ul>
+        
+            <a class="nav-item" href="#">GRAPHICS</a>
+            <a class="nav-item" href="#">UX DESIGN</a>
+            <img id="header-logo" src="https://www.rockyarcaroworks.com/idm250/wp-content/uploads/2024/02/Logo-Black-1.png" alt="logo">
+            <a class="nav-item" href="#">ABOUT ME</a>
+            <a class="nav-item" href="#">SHOP</a>
+        
+    </nav>
 </header>
