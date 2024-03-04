144f23498fc0522a38fc4a06b3c52bbfab9ce649
RockyArcaro <rockyarcaro3@gmail.com>
2024-03-04 00:12:13 -0500

    fix bugs

--- wp-content/themes/idm-250-Portfolio/footer.php
+++
@@ -1,7 +0,0 @@
-<footer style="background-color:black; color:white;"> 
-    &copy; 2024 | IDM 250
-</footer>
-
-</body>
-
-</html>
--- wp-content/themes/idm-250-Portfolio/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function theme_scripts_and_styles
-{
-    wp_enqueue_style('idm250-main-style', get_template_directory_uri() . 'wp-content/themes/idm-250-Portfolio/scripts/main.js');
-    wp_enqueue_script('idm250-main-script', get_template_directory_uri() . 'wp-content/themes/idm-250-Portfolio/scripts/main.js', [], false, ['in_footer' => true]);
-}
-
-add_action('wp_enqueue_scripts', 'theme_scripts_and_styles');
